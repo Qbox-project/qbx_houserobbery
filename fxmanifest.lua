6aef21ea5264aace916f9037049a2bc03ea0099b
DevX32 <60389339+DevX32@users.noreply.github.com>
2024-02-04 06:39:09 +0545

    lib locale

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,12 +5,11 @@ description 'qbx_houserobbery'
 repository 'https://github.com/Qbox-project/qbx_houserobbery'
 version '1.0.0'
 
+ox_lib 'locale'
+
 shared_scripts {
     '@ox_lib/init.lua',
     '@qbx_core/modules/lib.lua',
-    '@qbx_core/shared/locale.lua',
-    'locales/en.lua',
-    'locales/*.lua',
 }
 
 server_scripts {
